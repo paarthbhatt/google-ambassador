8931e3e74fa2eb21ff254efa3daff90fb3babc10
Parth Bhatt <paarthbhatt37@gmail.com>
2025-08-12 01:57:53 +0530

    added icons

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,9 +4,14 @@ import { GeistMono } from 'geist/font/mono'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
+  title: 'PARTH BHATT FOR AMBASSADOR',
+  description: 'Created with LOVE',
   generator: 'v0.dev',
+  icons: {
+    icon: 'https://www.google.com/favicon.ico',
+    shortcut: 'https://www.google.com/favicon.ico',
+    apple: 'https://www.google.com/favicon.ico',
+  },
 }
 
 export default function RootLayout({
@@ -17,6 +22,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <link rel="icon" type="image/x-icon" href="https://www.google.com/favicon.ico" />
+        <link rel="icon" type="image/png" sizes="32x32" href="https://www.gstatic.com/images/branding/product/1x/googleg_32dp.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="https://www.gstatic.com/images/branding/product/1x/googleg_16dp.png" />
+        <link rel="apple-touch-icon" href="https://www.gstatic.com/images/branding/product/1x/googleg_32dp.png" />
         <style>{`
 html {
   font-family: ${GeistSans.style.fontFamily};
